fe58bdb8da9eeea2214ecb99bf994e56da8bac22
Tushar Agrawal <tushar.code.inbox@gmail.com>
2024-12-25 18:25:59 +0530

    added wezterm and zsh configs

---
+++ .config/wezterm/constants.lua
@@ -0,0 +1,6 @@
+local M = {}
+
+M.bg_blurred = "C:/Users/TUSHAR/.config/wezterm/assets/bg-blurred.png"
+M.bg_image = M.bg_blurred
+
+return M
---
+++ .config/wezterm/wezterm.lua
@@ -0,0 +1,38 @@
+local wezterm = require("wezterm")
+local commands = require("commands")
+local constants = require("constants")
+
+local config = wezterm.config_builder()
+
+config.default_domain = 'WSL:Debian'
+
+-- Font settings
+--config.font_size = 19
+config.line_height = 1.2
+config.font = wezterm.font("JetBrains Mono")
+
+-- Colors
+config.colors = {
+	cursor_bg = "white",
+	cursor_border = "white",
+	indexed = { [239] = "lightslategrey" },
+}
+
+-- Appearance
+config.window_decorations = "RESIZE"
+config.hide_tab_bar_if_only_one_tab = true
+config.window_padding = {
+	left = 4,
+	right = 0,
+	top = '0.4cell',
+	bottom = '0.2cell',
+}
+config.window_background_image = constants.bg_image
+config.win32_system_backdrop = 'Acrylic'
+
+-- Custom commands
+wezterm.on("augment-command-palette", function()
+	return commands
+end)
+
+return config
